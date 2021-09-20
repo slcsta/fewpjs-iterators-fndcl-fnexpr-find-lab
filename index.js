b525c0846d5b195f06a4cbd6c33eb5a8cc736aa4
Sarah <sarahlchavez@gmail.com>
2021-09-20 05:44:30 -0700

    commit tree e9c18accd613ee2ae049ca3d72d0b7360ea10bc4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Write a function called superbowlWin
+// superbowlWin receives 1 arg, an array of JS objects
+// each object has 2 properties: year and result
+// use find() to test each object to see if the result is "W" (a win)
+// superbowlWin should return the year when the win occured
+// if no win found it should return undefined
 
+const superbowlWin = (arrayofobjects) => {
+
+    let result = arrayofobjects.find(arrayofobjects => arrayofobjects.result === "W")
+    return !!result ? result.year : undefined
+}
